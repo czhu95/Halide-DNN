103424390634ffd2a491f16b6b15260444ba1440
Chenzhuo Zhu <czhu95@stanford.edu>
2017-11-03 14:14:50 -0700

    Add type check.

--- src/test_conv.cc
+++ src/test_conv.cc
@@ -24,19 +24,24 @@ using namespace hdnn;
 
 int main(int argc, char* argv[]) {
     caffe::Net<float> caffe_net("cifar10_quick.prototxt", caffe::TEST);
-    auto& layers = caffe_net.layers();
-    auto& layer = layers[1];
-    auto& blobs = layer->blobs();
 
+    int layer_id = 1;
+    auto& layers = caffe_net.layers();
+    auto& layer = layers[layer_id];
+    auto param = layer->layer_param();
+    LOG_IF(INFO, Caffe::root_solver()) << param.type();
+    CHECK_EQ(param.type(), "Convolution");
+
+    auto& blobs = layer->blobs();
     caffe::caffe_rng_uniform<float>(blobs[0]->count(), 0, 1, blobs[0]->mutable_cpu_data());
     caffe::caffe_rng_uniform<float>(blobs[1]->count(), 0, 1, blobs[1]->mutable_cpu_data());
     LOG_IF(INFO, Caffe::root_solver()) << "Weight shape (Caffe): " << blobs[0]->shape_string();
 
-    auto* input_blob = caffe_net.input_blobs()[0];
-    auto* output_blob = caffe_net.top_vecs()[1][0];
+    auto* input_blob = caffe_net.bottom_vecs()[layer_id][0];
+    auto* output_blob = caffe_net.top_vecs()[layer_id][0];
     caffe::caffe_rng_gaussian<float>(input_blob->count(), 0, 1, input_blob->mutable_cpu_data());
 
-    caffe_net.Forward();
+    caffe_net.ForwardFromTo(layer_id, layer_id);
     LOG_IF(INFO, Caffe::root_solver()) << "Conv output shape (Caffe): " << output_blob->shape_string();
 
     auto conv_layer = hdnn::Conv2d<float>(input_blob->shape(1), output_blob->shape(1), 5, 1, 2);
@@ -59,7 +64,7 @@ int main(int argc, char* argv[]) {
         for (int c = 0; c < output_blob->shape(1); c ++)
             for (int h = 0; h < output_blob->shape(2); h ++)
                 for (int w = 0; w < output_blob->shape(3); w ++)
-                    CHECK_LT(std::abs(output_blob->data_at(n, c, h, w) - halide_output(w, h, c, n)) , 1e-5) << n << c << h << w;
+                    CHECK_LT(std::abs(output_blob->data_at(n, c, h, w) - halide_output(w, h, c, n)) , 1e-5) << n << " " << c << " " << h << " " << w;
 
     LOG_IF(INFO, Caffe::root_solver()) << "Passed.";
     return 0;
